801694b95779d769e1214877cfc4fec90a438838
Yoshihiro Kaneko <ykaneko0929@gmail.com>
2013-06-18 04:57:04 +0000

    fakevm: lb plugin: remove multi node emulation
    The bridge created by linuxbridge plugin is named by fixed prefix ('brq')
    plus network-id. Therefore, FakeVM linuxbridge plug-in cannot support
    the multi node emulation because it cannot avoid a bridge name conflicts.

--- quantum/fakevm/fakevm_agent_lb.py
+++ quantum/fakevm/fakevm_agent_lb.py
@@ -17,98 +17,49 @@
 # @author: Isaku Yamahata
 # @author: Yoshihiro Kaneko
 
-import time
+import sys
 
 from oslo.config import cfg
 
-from quantum.agent.linux import ip_lib
-from quantum.agent.linux import utils
-from quantum.agent.linux.interface import LinuxInterfaceDriver as linux_if
-from quantum.common import utils as q_utils
 from quantum.extensions import portbindings
-from quantum.fakevm import fakevm_agent
 from quantum.fakevm import fakevm_agent_plugin_base
 from quantum.openstack.common import log as logging
 from quantum.plugins.linuxbridge.common import config
 
 
+LOG = logging.getLogger(__name__)
+
+
 class QuantumFakeVMAgentLB(
         fakevm_agent_plugin_base.QuantumFakeVMAgentPluginBase):
-    _BRIDGE_PREFIX = 'qfbr-'
-    _PORT_PREFIX = 'qfp-'
 
     def __init__(self):
         super(QuantumFakeVMAgentLB, self).__init__()
         self.conf = None
         self.root_helper = None
-        self.interface_mappings = q_utils.parse_mappings(
-            cfg.CONF.LINUX_BRIDGE.physical_interface_mappings)
 
     def init(self, conf):
         super(QuantumFakeVMAgentLB, self).init(conf)
         self.vif_type = portbindings.VIF_TYPE_BRIDGE
+        # The bridge created by linuxbridge plugin is named by fixed prefix
+        # plus network-id. Therefore, this plug-in cannot support the multi
+        # node emulation because it cannot avoid a bridge name conflicts.
         if self.conf.FAKEVM.allow_multi_node_emulate:
-            self._init_bridge()
+            LOG.error(_('FakeVM linuxbridge plugin does not support '
+                        'the multi node emulation.'))
+            sys.exit(1)
 
     def cleanup(self):
-        self._cleanup_bridge()
+        pass
 
     def _get_vif_br_name(self, network_id, vif_uuid):
         return 'brq' + network_id[0:11]
 
-    def _get_tap_name(self, device):
-        return 'tap' + device[0:11]
-
-    def _get_hub_name(self, device):
-        return (self._BRIDGE_PREFIX + device)[:self.DEV_NAME_LEN]
-
-    def _get_port_name(self, device):
-        return (self._PORT_PREFIX + device)[:self.DEV_NAME_LEN]
-
-    def _execute(self, command):
-        return utils.execute(command, root_helper=self.root_helper)
-
-    def _bridge_exists(self, bridge_name):
-        try:
-            self._execute(['brctl', 'show', bridge_name])
-        except RuntimeError:
-            return False
-        return True
-
-    def _ensure_bridge(self, bridge_name):
-        if not self._bridge_exists(bridge_name):
-            if self._execute(['brctl', 'addbr', bridge_name]):
-                raise RuntimeError('brctl addbr %s failed' % bridge_name)
-            if self._execute(['brctl', 'setfd', bridge_name, str(0)]):
-                raise RuntimeError('brctl setfd %s 0 failed' % bridge_name)
-            if self._execute(['brctl', 'stp', bridge_name, 'off']):
-                raise RuntimeError('brctl stp %s off failed' % bridge_name)
-            if self._execute(['ip', 'link', 'set', bridge_name, 'up']):
-                raise RuntimeError('ip link set %s up failed' % bridge_name)
-        else:
-            time.sleep(1)       # XXX: race
-
-    def _init_bridge(self):
-        for physical_network in self.interface_mappings:
-            br_name = self._get_hub_name(physical_network)
-            self._ensure_bridge(br_name)
-            port_name = self._get_port_name(
-                self.interface_mappings[physical_network])
-            ip_wrapper = ip_lib.IPWrapper(self.root_helper)
-            if not ip_lib.device_exists(port_name, self.root_helper):
-                # ip link add $tunnel_interface type dummy
-                device = ip_wrapper.add_dummy(port_name)
-            else:
-                device = ip_wrapper.device(port_name)
-            device.link.set_up()
-            self._execute(['brctl', 'addif', br_name, port_name])
-
-    def _cleanup_bridge(self):
-        for physical_network in self.interface_mappings:
-            br_name = self._get_hub_name(physical_network)
-            port_name = self._get_port_name(
-                self.interface_mappings[physical_network])
-            self._execute(['brctl', 'delif', br_name, port_name])
+    def _get_veth_pair_names(self, vif_uuid):
+        # linuxbridge plugin agent adds the interface having a name which
+        # starts with 'tap' to a bridge
+        return ('tap' + vif_uuid[0:11],
+                ('qfv%s' % vif_uuid)[:self.DEV_NAME_LEN])
 
     def _make_vif_args(self, instance_id, network_id, vif_uuid, mac,
                        bridge_name):
@@ -123,15 +74,11 @@ class QuantumFakeVMAgentLB(
 
     def _probe_plug(self, network_id, vif_uuid, mac):
         br_veth_name, vm_veth_name = self._get_veth_pair_names(vif_uuid)
-        # linuxbridge plugin agent adds the interface having a name which
-        # starts with 'tap' to a bridge
-        br_veth_name = self._get_tap_name(vif_uuid)
         ns_name = self._get_ns_name(vif_uuid)
         self._create_probe(br_veth_name, vm_veth_name, mac, None, ns_name)
 
     def _probe_unplug(self, network_id, vif_uuid):
         br_veth_name, vm_veth_name = self._get_veth_pair_names(vif_uuid)
-        br_veth_name = self._get_tap_name(vif_uuid)
         br_name = self._get_vif_br_name(network_id, vif_uuid)
         ns_name = self._get_ns_name(vif_uuid)
         self._delete_probe(br_veth_name, vm_veth_name, br_name, ns_name)
